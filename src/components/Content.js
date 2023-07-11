c8637da2ce75a03444590b99cdf791120a854977
Amaya-2001 <amawickramasinghe2001@gmail.com>
2023-07-11 15:39:46 +0530

    created the react project and created content of the home page

---
+++ src/components/Content.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { FaLinkedin, FaFacebook } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import display from "../assets/3243637.jpg";
+
+function Content() {
+  return (
+    <div className="flex">
+      <div className="card w-96 bg-base-100 shadow-xl ml-60 mt-20">
+        <div className="card-body items-center text-center">
+          <h2 className="card-title text-4xcl">👋Hello, It's Me</h2>
+          <div className="card-title text-2xl italic"> Amaya</div>
+          <p>
+            An Undergraduate software Engineer at the Open University of Sri
+            Lanka. I possess a strong aptitude for adapting to various
+            technologies and enjoy navigating the ever-evolving world of
+            technology. I'm excited to share my projects and experiences with
+            you.
+          </p>
+          <button class="btn rounded-full mt-5 btn-primary btn-outline">
+            <FaLinkedin size="25" />
+            <MdEmail size="25" />
+            <FaFacebook size="25" />
+          </button>
+          <div className="card-actions mt-5">
+            <button className="btn btn-primary">Download My Resume</button>
+          </div>
+        </div>
+      </div>
+      <div className="carousel carousel-end rounded-box mt-20 ml-10 mr-5">
+        <div className="carousel-item">
+          <img src={display} alt="Drink" width="300 px" />
+        </div>
+        <div className="carousel-item">
+          <img src={display} alt="Drink" width="300 px" />
+        </div>
+        <div className="carousel-item">
+          <img src={display} alt="Drink" width="300 px" />
+        </div>
+      </div>
+      {/* <img
+        className="mt-40 ml-20"
+        src={display}
+        alt="Album"
+        width="565 px"
+        // height="765 px"
+      /> */}
+    </div>
+  );
+}
+
+export default Content;
---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./Navbar.js";
+import Content from "./Content.js";
+
+function Home() {
+  return (
+    <div>
+      <Navbar />
+      <Content />
+    </div>
+  );
+}
+
+export default Home;
