4a3bc8c1f68e6fb602fdb452f8d8555a2b8500cd
Amaya-2001 <amawickramasinghe2001@gmail.com>
2023-07-13 14:09:22 +0530

    created certificate file to add my achivements

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./Navbar.js";
 import Content from "./Content.js";
+import Certificate from "./Certificate.js";
 
 function Home() {
   return (
     <div>
       <Navbar />
       <Content />
+      <Certificate />
     </div>
   );
 }
