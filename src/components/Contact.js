fe5aa89e0499631d2c4817e113d838616c046a77
Amaya-2001 <amawickramasinghe2001@gmail.com>
2023-07-19 10:56:53 +0530

    Finished the contact page by adding contact details

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,25 +2,33 @@ import React, { useState } from "react";
 import Navbar from "./Navbar";
 import { MdEmail, MdLocationOn } from "react-icons/md";
 import { FaFacebook, FaLinkedin } from "react-icons/fa";
-// import profile from "../assets/Capture2.PNG";
-// import email from "../assets/gmail_icon-icons.com_62758.png";
-// import linkedin from "../assets/linkedin-logo-linkedin-icon-transparent-free-png.webp";
-// import facebook from "../assets/Facebook-logo.png";
+import profile from "../assets/Capture2.png";
 
 function Contact() {
+  const openInNewTab = (url) => {
+    window.open(url, "_blank", "noreferrer");
+  };
   return (
-    <div>
+    <div className="bg-gradient-to-r from-violet-950 via-purple-500 to-pink-500 ... h-screen ....">
       <Navbar />
-      <div class="flex justify-center ... mt-20 mb-20">
+      <div class="flex justify-center ... mt-20">
         <div className="card lg:card-side bg-base-100 shadow-xl">
           <div>
             <h1 className="text-7xl font-mono mt-5 ml-5 ">Contact Me</h1>
+            <img src={profile} alt="profile" className=" ml-20" />
           </div>
           <div className="card-body">
-            {/* <h2 className="card-title">Shoes!</h2> */}
             <div class="flex justify-center ... mt-10">
               <div>
-                <button className="btn btn-outline btn-primary w-96 rounded-full lowercase ...">
+                <button
+                  className="btn btn-outline btn-primary w-96 rounded-full lowercase ..."
+                  role="link"
+                  onClick={() =>
+                    openInNewTab(
+                      "mailto:amawickramasinghe2001@gmail.com?subject=Subject&body=Body%20goes%20here"
+                    )
+                  }
+                >
                   <MdEmail size="25px" />
                   <p>amawickramasinghe2001@gmail.com</p>
                 </button>
@@ -28,7 +36,15 @@ function Contact() {
             </div>
             <div class="flex justify-center ... mt-10">
               <div>
-                <button className="btn btn-outline btn-primary w-96 rounded-full capitalize ...">
+                <button
+                  className="btn btn-outline btn-primary w-96 rounded-full capitalize ..."
+                  role="link"
+                  onClick={() =>
+                    openInNewTab(
+                      "https://www.linkedin.com/in/amaya-wickramasinghe-6378a6231/"
+                    )
+                  }
+                >
                   <FaLinkedin size="25px" />
                   <p>Amaya Wickramasinghe</p>
                 </button>
@@ -36,7 +52,15 @@ function Contact() {
             </div>
             <div class="flex justify-center ... mt-10">
               <div>
-                <button className="btn btn-outline btn-primary w-96 rounded-full lowercase ...">
+                <button
+                  className="btn btn-outline btn-primary w-96 rounded-full lowercase ..."
+                  role="link"
+                  onClick={() =>
+                    openInNewTab(
+                      "https://www.facebook.com/profile.php?id=100090623790910"
+                    )
+                  }
+                >
                   <FaFacebook size="25px" />
                   <p>https://www.facebook.com/</p>
                 </button>
@@ -44,7 +68,13 @@ function Contact() {
             </div>
             <div class="flex justify-center ... mt-10">
               <div>
-                <button className="btn btn-outline btn-primary w-96 rounded-full capitalize ...">
+                <button
+                  className="btn btn-outline btn-primary w-96 rounded-full capitalize ..."
+                  role="link"
+                  onClick={() =>
+                    openInNewTab(" https://maps.app.goo.gl/yScKo8ULruk8CUCGA")
+                  }
+                >
                   <MdLocationOn size="25px" />
                   <p>Wadduwa, Kalutara</p>
                 </button>
